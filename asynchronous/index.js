c0be5c678ada86b4feb213c7af569558e0905690
K Harsh <harshnaidu2u@gmail.com>
2023-11-10 22:08:39 +0530

    waiting for multiple promises

--- asynchronous/index.js
+++ asynchronous/index.js
@@ -24,16 +24,46 @@ const getDogPic = async() => {
         const dogBreed = await readFilePro(`${__dirname}/dog.txt`);
         console.log(`Breed: ${dogBreed}`);
     
-        const res = await superagent.get(`https://dog.ceo/api/breed/${dogBreed}/images/random`);
-        console.log(res.body.message);
+        const res1Pro = await superagent.get(`https://dog.ceo/api/breed/${dogBreed}/images/random`);
+        const res2Pro = await superagent.get(`https://dog.ceo/api/breed/${dogBreed}/images/random`);
+        const res3Pro = await superagent.get(`https://dog.ceo/api/breed/${dogBreed}/images/random`);
+
+        const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+        const imgs = all.map(el => 
+            el.body.message
+        );
+        console.log(imgs);
     
-        await writeFilePro('dog-img.txt', res.body.message);
-        console.log('randomdog image saved to file');
+        await writeFilePro('dog-img.txt', imgs.join('/n'));
+        console.log('random dog image saved to file');
     } catch(err) {
-        console.log(err.message);
+        console.log(err);
+
+        throw err;
     }
+
+    return '2: READY 🐶';
 };
-getDogPic();
+
+// using async await to handle promises
+(async() => {
+    try {
+        console.log('1: Will get dog pics!');
+        const x = await getDogPic();
+        console.log(x);
+        console.log('3: Done getting dog pics!');
+      } catch (err) {
+        console.log('ERROR 💥');
+      }
+})();
+
+// handling promises with then and catch
+/*
+getDogPic().then((x) => {
+    console.log(x);
+    console.log('3: done getting dog pics');
+}).catch(err => console.log('ERROR 💥'));
+*/
 
 /*
 readFilePro(`${__dirname}/dog.txt`)
