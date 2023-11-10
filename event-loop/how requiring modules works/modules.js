d7578c7289cfaa0e830e3963a5216dda0f073375
K Harsh <harshnaidu2u@gmail.com>
2023-11-10 14:38:06 +0530

    caching

--- event-loop/how requiring modules works/modules.js
+++ event-loop/how requiring modules works/modules.js
@@ -13,3 +13,10 @@ console.log(calac1.multiply(9, 9));
 // using destructring
 const { add, multiply, divide, subtract} = require("./test-module-2");
 console.log(add(9, 9));
+
+// caching
+require("./test-module-3")();
+
+// other 2 logged again came from cache ehiche is stored somewhere in nodes processes cache.
+require("./test-module-3")();
+require("./test-module-3")();
---
+++ event-loop/how requiring modules works/test-module-3.js
@@ -0,0 +1,4 @@
+console.log("hello!!!!");
+// this module loaded once, will executed once because of caching.
+
+module.exports = console.log("logged again");
