bcac0a80b3350e9481ea9a26388180bb726249a1
K Harsh <harshnaidu2u@gmail.com>
2023-11-10 14:24:06 +0530

    exports

--- event-loop/how requiring modules works/modules.js
+++ event-loop/how requiring modules works/modules.js
@@ -1,6 +1,15 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
+// module.exports
 const C = require("./test-module");
 const calac1 = new C();
-console.log(calac1.divide(9, 9));
+console.log(calac1.multiply(9, 9));
+
+// exports
+// const calac2 = require("./test-module-2");
+// console.log(calac2.add(9, 9));
+
+// using destructring
+const { add, multiply, divide, subtract} = require("./test-module-2");
+console.log(add(9, 9));
